dcb21cca7f3b438073c4d3155386194df5615bd7
paska <paska@avast.com>
2015-10-26 08:53:42 +0100

    Initial Commit

---
+++ Portfolio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Portfolio
+//
+//  Created by Radovan on 26/10/15.
+//  Copyright © 2015 just Radovan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
