e72527e769f26e814ee086988617902004e676ee
Radovan Paška <paska@avast.com>
2015-11-15 20:07:50 +0100

    + Custom map pin on photo detail view.

--- Portfolio/PhotoDetailViewController.swift
+++ Portfolio/PhotoDetailViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 import MapKit
 import AlamofireImage
 
-class PhotoDetailViewController: UIViewController {
+class PhotoDetailViewController: UIViewController, MKMapViewDelegate {
 
+    // MARK: Properties - location
     let locationManager: CLLocationManager!
     let dataController = DataController()
     let mapRadius: CLLocationDistance = 150 // Metres?
+    let annotationViewReuseID = "mapPin"
+    
+    // MARK: Properties - photo
     var photo: PhotoModel?
     var imageCache: AutoPurgingImageCache
     var imageDownloader: ImageDownloader
@@ -41,6 +45,8 @@ class PhotoDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mapView.delegate = self
         
         if let photo = photo {
             self.title = photo.title
@@ -79,6 +85,62 @@ class PhotoDetailViewController: UIViewController {
             locationManager.requestWhenInUseAuthorization()
         } else {
             mapView.showsUserLocation = true
+        }
+    }
+    
+    // Handle adding annotation view to the map.
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        if (annotation is MKUserLocation) {
+            return nil
+        }
+        
+        var pin = mapView.dequeueReusableAnnotationViewWithIdentifier(annotationViewReuseID)
+        var button: UIButton?
+        
+        if (pin != nil) {
+            button = pin!.leftCalloutAccessoryView as? UIButton
+            
+            pin!.annotation = annotation
+        } else {
+            button = UIButton(type: UIButtonType.Custom)
+            button!.frame.size.width = 46
+            button!.frame.size.height = 46
+            
+            pin = MKAnnotationView(annotation: annotation, reuseIdentifier: annotationViewReuseID)
+            pin!.image = UIImage(named: "Pin Photo")
+            pin!.leftCalloutAccessoryView = button
+            pin!.canShowCallout = true
+        }
+        
+        if let button = button {
+            displayThumbnail(button)
+        }
+        
+        return pin
+    }
+    
+    // Load and display photo thumbnail.
+    // Use cache when possible.
+    func displayThumbnail(button: UIButton) {
+        if (photo == nil) {
+            return
+        }
+        
+        let url = photo!.getThumbnailForSize(.PIN)?.url
+        if (url == nil) {
+            return
+        }
+        
+        // Set thumbnail params
+        let request = NSURLRequest(URL: NSURL(string: url!)!)
+        let size = CGSize(width: 46.0, height: 46.0)
+        let filter = AspectScaledToFillSizeWithRoundedCornersFilter(size: size, radius: 6.0)
+        
+        // Load & store image.
+        imageDownloader.downloadImage(URLRequest: request, filter: filter) { response in
+            if let image: UIImage = response.result.value {
+                button.setImage(image, forState: .Normal)
+            }
         }
     }
     
