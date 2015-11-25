34186a6014ed6c7fcfe77924d77b09d3b742d1ff
Radovan Paška <paska@avast.com>
2015-11-25 17:39:08 +0100

    + Add iPhone 6 lens to the list.

--- Portfolio/LensList.swift
+++ Portfolio/LensList.swift
@@ -14,6 +14,7 @@ class LensList {
         "E 32mm F1.8": "Zeiss Touit 32/1.8",
         "E 19mm F2.8": "Sigma 19/2.8",
         "E 50mm F1.8 OSS": "Sony 50/1.8",
-        "E 18-55mm F3.5-5.6 OSS": "Sony 18-55/3.5-5.6"
+        "E 18-55mm F3.5-5.6 OSS": "Sony 18-55/3.5-5.6",
+        "iPhone 6 back camera 4.15mm f/2.2": "iPhone 6 4/2.2"
     ]
 }
